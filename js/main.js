c01d77f14d35f44d901177a0b3e7bd9371ef40f8
Alexandre Costa <xanocosta.87@hotmail.com>
2014-10-16 10:19:20 +0100

    Alteramos a configuraçao da velocidade do boneco

--- js/main.js
+++ js/main.js
@@ -1,32 +1,37 @@
 $(document).ready(function(){
+	speed= 20;
 
 	$('#btright').click(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
-		newpleft = position.left +20;
+		newpleft = position.left +speed;
 		if (newpleft > 800) {
 			newpleft = 800;
 		}
-		$b.css('left', newpleft + 'px');
+		//$b.css('left', newpleft + 'px');
+		$b.animate({"left": "+=" + speed + "px"},"slow");
 	})
 
 	$('#btleft').click(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
-		newpleft = position.left -20;
-		if (newpleft < 40) {
-			newpleft = 40
+		newpleft = position.left -speed;
+		if (newpleft > 40) {
+			$b.animate({"left": "-=" + speed + "px"},"slow");
 		}
-		$b.css('left', newpleft + 'px');
+		//$b.css('left', newpleft + 'px');
+
 	})
 
 	$('#btdown').click(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
-		newpleft = position.top +10;
+		newpleft = position.top +speed;
 		$b.css('top', newpleft + 'px');
+
 		if (newpleft > 300) {
-			newpleft = 300
+			
+			$b.animate({"left": "-=" + speed + "px"},"slow");
 
 	}
 	})
@@ -34,7 +39,7 @@ $(document).ready(function(){
 	$('#btup').click(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
-		newpleft = position.top -10;
+		newpleft = position.top -speed;
 		$b.css('top', newpleft + 'px');
 		if (newpleft > 0) {
 			newpleft = 0
